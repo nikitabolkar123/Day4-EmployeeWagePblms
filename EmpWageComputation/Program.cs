268b56ddf557ebd6a02a7bb04bc97379229d4321
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-08 03:27:38 -0800

    Added UC

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int value = r.Next(0, 2);//only generate values bt 0-2 means 0,1 ;
+            // 0 is for present;
+            // 1 is for absent;
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
