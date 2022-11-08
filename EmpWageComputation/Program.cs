38c2f539670b95fd0d36fa274ad40826b419cb20
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-08 03:39:02 -0800

    Added UC

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -7,27 +7,31 @@ namespace EmpWageComputation
         public const int isPartTime = 1;
         public const int isFullTime = 2;
         public const int empRatePerHr = 20;
+        public const int noOfWorkingDays = 2;
         static void Main(string[] args)
         {
-            int empHr = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int empHr = 0, empWage = 0, totalWage = 0;
+            for (int day = 0; day <= noOfWorkingDays; day++)
             {
-                case isPartTime:
-                    empHr = 4;
-                    break;
-                case isFullTime:
-                    empHr = 8;
-                    break;
-                default:
-                    empHr = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHr = 4;
+                        break;
+                    case isFullTime:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                empWage = empHr * empRatePerHr;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Emp Wage=" + empWage);
             }
-            empWage = empHr * empRatePerHr;
-
-            Console.WriteLine("EmpWage=" + empWage);
+            Console.WriteLine("total Wage =" + totalWage);
         }
     }
 }
